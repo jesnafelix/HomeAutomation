468e08cee0393bdea0570a12888587398a2aad59
Jesna Felix <jesnafelix@gmail.com>
2023-11-20 23:23:40 -0500

    Page1Gui updated

--- HomeAutomationClient/src/exxx/Page1Gui.java
+++ HomeAutomationClient/src/exxx/Page1Gui.java
@@ -5,8 +5,6 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
-import java.awt.CardLayout;
-
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -31,7 +29,6 @@ public class Page1Gui extends JFrame{
 private JTextField textField;
 private JTextField textField_1;
 private JTextField textField_2;
-private CardLayout cardLayout;
 
 
 
@@ -124,31 +121,43 @@ JPanel panel_1 = new JPanel();
 panel_1.setBackground(new Color(128, 128, 128));
 panel_1.setBounds(10, 11, 863, 103);
 getContentPane().add(panel_1);
-panel_1.setLayout(new CardLayout(0, 0));
+panel_1.setLayout(null);
+
+textField = new JTextField();
+textField.setBounds(161, 0, 181, 43);
+panel_1.add(textField);
+textField.setFont(new Font("Dialog", Font.PLAIN, 20));
+textField.setColumns(10);
 
 textField_1 = new JTextField();
-panel_1.add(textField_1, "name_3273862644000");
+textField_1.setBounds(493, 0, 181, 43);
+panel_1.add(textField_1);
 textField_1.setFont(new Font("Dialog", Font.PLAIN, 20));
 textField_1.setColumns(10);
 
 JLabel lblNewLabel = new JLabel("IP Address");
-panel_1.add(lblNewLabel, "name_3273899727400");
+lblNewLabel.setBounds(59, 7, 101, 29);
+panel_1.add(lblNewLabel);
 lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
 
 JLabel lblPortNumber = new JLabel("Port Number");
-panel_1.add(lblPortNumber, "name_3273938073200");
+lblPortNumber.setBounds(362, 7, 124, 29);
+panel_1.add(lblPortNumber);
 lblPortNumber.setFont(new Font("Dialog", Font.PLAIN, 20));
 
 JButton btnNewButton = new JButton("CONNECT");
-panel_1.add(btnNewButton, "name_3273979158400");
+btnNewButton.setBounds(478, 67, 138, 29);
+panel_1.add(btnNewButton);
 btnNewButton.setFont(new Font("Dialog", Font.PLAIN, 20));
 
 JButton btnDisconnect = new JButton("DISCONNECT");
-panel_1.add(btnDisconnect, "name_3274014528300");
+btnDisconnect.setBounds(647, 67, 175, 29);
+panel_1.add(btnDisconnect);
 btnDisconnect.setFont(new Font("Dialog", Font.PLAIN, 20));
 
 textField_2 = new JTextField();
-panel_1.add(textField_2, "name_3274053379900");
+textField_2.setBounds(36, 54, 219, 37);
+panel_1.add(textField_2);
 textField_2.setFont(new Font("Dialog", Font.PLAIN, 20));
 textField_2.setColumns(10);
 
@@ -158,11 +167,6 @@ getContentPane().add(tabbedPane);
 
 JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
 tabbedPane.addTab("Login", null, tabbedPane_1, null);
-
-textField = new JTextField();
-tabbedPane_1.addTab("New tab", null, textField, null);
-textField.setFont(new Font("Dialog", Font.PLAIN, 20));
-textField.setColumns(10);
 
 JTabbedPane tabbedPane_2 = new JTabbedPane(JTabbedPane.TOP);
 tabbedPane.addTab("Device", null, tabbedPane_2, null);
